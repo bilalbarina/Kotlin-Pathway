6d4843518eee784c13210b60ada19b77203ab07a
bilalbarina <dev.barina@gmail.com>
2023-02-17 13:32:15 +0100

    Added image view for the dice roll

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -16,15 +17,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
+        // Create new dice object with 6 sides
         val dice = Dice(6)
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = dice.roll().toString()
+
+        // Roll the dice
+        val diceRoll: Int = dice.roll()
+
+        // Find the image that represents the dice
+        val diceImageRes = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Set Image based on the dice roll
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(diceImageRes)
+        // Add image description for screen readers
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
-class Dice(private val numSlides: Int) {
+class Dice(private val numSides: Int) {
     fun roll(): Int {
-        return (1..numSlides).random()
+        return (1..numSides).random()
     }
 }
 
