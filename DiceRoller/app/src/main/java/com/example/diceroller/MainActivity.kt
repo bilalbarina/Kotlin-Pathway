66565ac69f9d56f5e25dbeccdbbca9b34ff4d337
bilalbarina <dev.barina@gmail.com>
2023-02-17 12:52:35 +0100

    Added roll dice logic

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener { this.rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = dice.roll().toString()
     }
 }
+
+class Dice(private val numSlides: Int) {
+    fun roll(): Int {
+        return (1..numSlides).random()
+    }
+}
+
