e34887b065c14b49ee83cb0eb0056bd6af1c6b09
bilalbarina <dev.barina@gmail.com>
2023-02-20 12:06:18 +0100

    Added calculate tip functionality

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Calculate tip on click
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val costOfServiceText = binding.costOfService.text.toString()
+        val costOfService: Double? = costOfServiceText.toDoubleOrNull()
+
+        if (costOfService == null) {
+            Toast.makeText(this, "Cost of service is required", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_amazing -> 0.20
+            R.id.option_good -> 0.18
+            else -> 0.15
+        }
+        var tip: Double = costOfService * tipPercentage
+        if (binding.roundUpTip.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip: String = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
